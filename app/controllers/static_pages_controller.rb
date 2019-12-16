eefab63ef1402cd731469d9d61cd1d9bc11c9f39
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-12-16 18:55:20 +0700

    add layouts, add search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,16 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
+
+  def search
+    if params[:search].blank?  
+      @results = []
+    else 
+      @parameter = params[:search].downcase  
+      @results = Item.all.where("lower(name) LIKE :search", search: "%#{@parameter}%") 
+    end
+  end
 end
